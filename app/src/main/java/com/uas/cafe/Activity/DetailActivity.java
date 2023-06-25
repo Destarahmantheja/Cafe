e7d92e3c889a236592e9a9d6b6e3dd03e4431dcb
desta <destarahmantheja@gmail.com>
2023-06-25 13:35:31 +0700

    update againnnn

--- app/src/main/java/com/uas/cafe/Activity/DetailActivity.java
+++ app/src/main/java/com/uas/cafe/Activity/DetailActivity.java
@@ -8,15 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.uas.cafe.R;
 
 public class DetailActivity extends AppCompatActivity {
-    private TextView tvId, tvNama, tvAlamat, tvDeskripsi, tvRating, tvKoordinat;
-    private String detId, detNama, detAlamat, detDeskripsi, detRating, detKoordinat;
+    private TextView tvNama, tvAlamat, tvDeskripsi, tvRating, tvKoordinat;
+    private String detNama, detAlamat, detDeskripsi, detRating, detKoordinat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        tvId = findViewById(R.id.tv_id);
         tvNama = findViewById(R.id.tv_nama);
         tvAlamat = findViewById(R.id.tv_alamat);
         tvDeskripsi = findViewById(R.id.tv_deskripsi);
@@ -25,15 +24,13 @@ public class DetailActivity extends AppCompatActivity {
 
 
         Intent terima = getIntent();
-        detId = terima.getStringExtra("xId");
         detNama = terima.getStringExtra("xNama");
         detAlamat = terima.getStringExtra("xAlamat");
         detDeskripsi = terima.getStringExtra("xDeskripsi");
         detRating = terima.getStringExtra("xRating");
         detKoordinat = terima.getStringExtra("xKoordinat");
 
-
-        tvId.setText(detId);
+        
         tvNama.setText(detNama);
         tvAlamat.setText(detAlamat);
         tvDeskripsi.setText(detDeskripsi);
